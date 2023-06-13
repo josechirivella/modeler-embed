b2027ba6d0f51ec89d00462aadad33f67a43eab0
Jose Chirivella <jose.chirivella@processmaker.com>
2023-06-13 14:57:41 -0500

    Added Modeler

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Modeler />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Modeler } from "@processmaker/modeler";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Modeler,
+  },
+};
 </script>
 
 <style>
